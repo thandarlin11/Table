1fbdcf53d53d771df83fcbe5ee8be4771dc8191b
thandarlin11 <thandarlin@easysheets.net>
2022-06-24 10:14:19 +0630

    first commit

---
+++ src/EditableRow.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const EditableRow = ({editFormData, handleEditFormChange, handleCancelClick}) => {
+  return (
+    <tr>
+      <td>
+        <input 
+        type= "text" 
+        name="fullName" 
+        required ="required" 
+        placeholder="Enter a name...."
+        value={editFormData.fullName}
+        onChange={handleEditFormChange}
+        />
+      </td>
+      <td>
+        <input 
+        type= "text" 
+        name="address" 
+        required ="required" 
+        placeholder="Enter an address...."
+        value={editFormData.address}
+        onChange={handleEditFormChange}
+        />
+      </td>
+      <td>
+        <input type= "text" 
+        name="phoneNumber" 
+        required ="required" 
+        placeholder="Enter a phone aumber...."
+        value={editFormData.phoneNumber}
+        onChange={handleEditFormChange}
+        />
+      </td>
+      <td>
+        <input type= "email" 
+        name="email" 
+        required ="required" 
+        placeholder="Enter an email...."
+        value={editFormData.email}
+        onChange={handleEditFormChange}
+        />
+      </td>
+      <td>
+        <button type="submit">Save</button>
+        <button type="button" 
+        onClick={handleCancelClick}>
+        Cancel</button>
+      </td>
+    </tr>
+  );
+};
+
+export default EditableRow;
